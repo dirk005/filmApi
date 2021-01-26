bd7196039653ca7909488438fd96cc622acbd0a4
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-01-26 10:45:34 +0200

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const helmet = require(helmet);
+const helmet = require("helmet");
 require("dotenv").config();
 
 //Import Routes
